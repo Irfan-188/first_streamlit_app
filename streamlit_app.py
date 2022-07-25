b6f2bddcdcbc72c2fef23d5899e48f7b8e350a93
Irfan Ali Shaik <109959418+Irfan-188@users.noreply.github.com>
2022-07-25 17:25:17 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title('my parents new healthy diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('One puri')
+streamlit.text('chapathi & mutton')
+streamlit.text('Idli and sambar')
