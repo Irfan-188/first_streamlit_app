1f3c49be13ba326cf3ef4d3667706a54eeba0e56
Irfan Ali Shaik <109959418+Irfan-188@users.noreply.github.com>
2022-07-26 10:56:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,6 @@ streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text(' 🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text(' 🐔strHard-Boiled Free-Range Egg')
 streamlit.text(' 🥑🍞avacado toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
